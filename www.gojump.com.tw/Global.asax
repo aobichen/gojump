8cd376b00ac597ec5272c363ca31e2ccb0b2da04
aobi <aobichen@gmail.com>
2017-02-05 10:52:00 +0800

    first commit

---
+++ www.gojump.com.tw/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="www.gojump.com.tw.MvcApplication" Language="C#" %>
---
+++ www.gojump.com.tw/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(www.gojump.com.tw.Startup))]
+namespace www.gojump.com.tw
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
